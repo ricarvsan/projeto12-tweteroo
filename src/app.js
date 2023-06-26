54fc82276d83ecdca9a8c186ebae7f4e30a386e0
ricarvsan <ricarvsan@gmail.com>
2023-06-25 21:12:49 -0300

    post /tweets e get /tweets

--- src/app.js
+++ src/app.js
@@ -10,20 +10,40 @@ const usuarios = []
 const tweets = []
 
 app.post('/sign-up', (req, res) => {
-
     usuarios.push(req.body)
-    console.log(usuarios)
     res.send('OK')
 })
 
 app.post('/tweets', (req, res) => {
-    console.log(req.body)
+    const { username, tweet } = req.body
+
+    if (usuarios.find(obj => obj.username === username)) {
+        tweets.push(tweet)
+        return res.send('OK')
+    } else {
+        return res.send('UNAUTHORIZED')
+    }
 })
 
 app.get('/tweets', (req, res) => {
-    console.log(req.body)
-})
+    let last10Tweets = []
 
+    for (let i = 0; i < tweets.length; i++) {
+        last10Tweets.push(
+            {
+                username: usuarios[0].username,
+                avatar: usuarios[0].avatar,
+                tweet: tweets[i]
+            }
+        )
+    }
+
+    if(last10Tweets.length > 10) {
+        last10Tweets = last10Tweets.slice(-10)
+    }
+
+    res.send(last10Tweets)
+}) 
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`O Servidor está rodando na porta ${PORT}`))
